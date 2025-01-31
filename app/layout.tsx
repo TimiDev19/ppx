fb1603664ff253df457a54ae5c6ed4d063ce7ca0
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-01-31 17:57:25 +0100

    favicon.io

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="shortcut icon" href="/earplug-logo.png" type="image/x-icon" />
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-[100vh] overflow-y-scroll scroll-smooth`}
       >
