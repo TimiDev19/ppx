5e891a47f864a1252b3e0426c06066d6be599617
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2025-02-18 14:42:29 +0100

    links added to hero

--- components/Homepage/sections/Hero.tsx
+++ components/Homepage/sections/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from 'react'
 import { CopyAll } from '@mui/icons-material';
+import Link from 'next/link';
 
 const Hero = () => {
     const [copied, setCopied] = useState(false);
@@ -37,6 +38,29 @@ const Hero = () => {
                     part of a project that prioritizes <i>transparency,</i> <i>security and growth</i>. Buy and hold $HIC and let's build a stronger future
                     for all of us.
                 </p>
+
+                <div className=" flex flex-col w-[90%] mx-auto items-center justify-center lg:flex-row">
+                    <Link
+                        href={"https://t.me/+lPULoNfMXy85N2Ex"}
+                        className=' mx-3 mb-4 bg-slate-800 text-white p-3 rounded-md font-bold hover:shadow-sm hover:shadow-white duration-500'
+                    >
+                        Join Our Telegram
+                    </Link>
+
+                    <Link
+                        href={"https://discord.gg/JaD3jtzB"}
+                        className=' mx-3 mb-4 bg-slate-800 text-white p-3 rounded-md font-bold hover:shadow-sm hover:shadow-white duration-500'
+                    >
+                        Join Our Discord
+                    </Link>
+
+                    <Link
+                        href={"https://x.com/hostageincrypto?s=21&t=nk5kCymf71qnB1WAw2u8Zg"}
+                        className=' mx-3 mb-4 bg-slate-800 text-white p-3 rounded-md font-bold hover:shadow-sm hover:shadow-white duration-500'
+                    >
+                        Follow Us On X
+                    </Link>
+                </div>
 
                 <div onClick={handleCopy} className=' flex flex-col items-center justify-center bg-slate-800 text-white py-1 w-[95%] mx-auto lg:mx-0 px-4 rounded-md'>
                     <h1 className=" mr-4 cursor-pointer font-extrabold text-white break-words break-all overflow-wrap text-md md:text-xl text-center lg:text-3xl max-w-full">
